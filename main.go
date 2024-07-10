2160c36c179823ca696d148e4634bc284ff54c1e
Kyota Okabe <enrico.fermi.phys@gmail.com>
2024-07-10 00:10:11 +0000

    feat: implement ptrace cont

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("hello")
+	cmd := exec.Command("/home/kyota/src/godbg/hello")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Ptrace: true,
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("failed to start command: %s", err)
+	}
+
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatalf("failed to wait process: %s", err)
+	}
+
+
+	pid := cmd.Process.Pid
+	syscall.PtraceCont(pid, 0)
+
+	// do something
+	fmt.Println("process has been completed.")
 }

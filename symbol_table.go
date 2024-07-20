944ee855427e6ba2944bd336966b355b01af55ba
Kyota Okabe <enrico.fermi.phys@gmail.com>
2024-07-20 13:11:07 +0000

    feat: set breakpoint at function

--- symbol_table.go
+++ symbol_table.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"debug/dwarf"
 	"debug/elf"
 	"debug/gosym"
 	"errors"
+	"fmt"
 )
+
+type SymbolTable struct {
+	table     *gosym.Table
+	dwarfData *dwarf.Data
+}
 
 // section is described in the elf format document.
 // @see http://www.skyfree.org/linux/references/ELF_Format.pdf
 //
 // you can see how to use gosym in pclntab_test
 // @see https://cs.opensource.google/go/go/+/refs/tags/go1.22.5:src/debug/gosym/pclntab_test.go;l=86
-func NewSymbolTable(debugeePath string) (*gosym.Table, error) {
+func NewSymbolTable(debugeePath string) (*SymbolTable, error) {
 	f, err := elf.Open(debugeePath)
 	if err != nil {
 		return nil, err
 	}
-
 	defer f.Close()
 
 	s := f.Section(".gosymtab")
@@ -36,5 +42,72 @@ func NewSymbolTable(debugeePath string) (*gosym.Table, error) {
 
 	pcln := gosym.NewLineTable(pclndata, f.Section(".text").Addr)
 
-	return gosym.NewTable(symdata, pcln)
+	table, err := gosym.NewTable(symdata, pcln)
+	if err != nil {
+		return nil, err
+	}
+
+	dwarfData, err := f.DWARF()
+	if err != nil {
+		return nil, err
+	}
+
+	return &SymbolTable{
+		table:     table,
+		dwarfData: dwarfData,
+	}, nil
+}
+
+func (st *SymbolTable) PCToLine(pc uint64) (file string, line int, fn *gosym.Func) {
+	return st.table.PCToLine(pc)
+}
+
+func (st *SymbolTable) PCToFunc(pc uint64) *gosym.Func {
+	return st.table.PCToFunc(pc)
+}
+
+func (st *SymbolTable) LookupFunc(funcname string) (*gosym.Func, error) {
+	fn := st.table.LookupFunc(funcname)
+	if fn == nil {
+		return nil, fmt.Errorf("failed to look up function: %s", funcname)
+	}
+
+	return fn, nil
+}
+
+func (st *SymbolTable) GetPrologueEndAddress(fn *gosym.Func) (uint64, error) {
+	reader := st.dwarfData.Reader()
+	for {
+		entry, err := reader.Next()
+		if err != nil {
+			break
+		}
+
+		if entry.Tag != dwarf.TagCompileUnit {
+			continue
+		}
+
+		lineReader, err := st.dwarfData.LineReader(entry)
+		if err != nil {
+			return 0, err
+		}
+
+		var lineEntry dwarf.LineEntry
+		for {
+			if err := lineReader.Next(&lineEntry); err != nil {
+				break
+			}
+
+			if lineEntry.Address == fn.Entry {
+				for err := lineReader.Next(&lineEntry); err == nil; {
+					if lineEntry.PrologueEnd {
+						return lineEntry.Address, nil
+					}
+				}
+			}
+		}
+
+	}
+
+	return 0, fmt.Errorf("faield to get prologue end address for function %s", fn.Name)
 }
